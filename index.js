9e2efc2927854a8dddaf1ad810d04121c7958f99
Nick Strayer <nick.strayer@gmail.com>
2018-11-29 15:23:04 -0600

    Relative positions of tooltips now stay between resizings.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 // r2d3: https://rstudio.github.io/r2d3
 //
 
+
 const svg = div.selectAppend('svg').at({width,height});
 // These aren't shown in the tooltip. 
 const ommitted_props = ['x', 'y', 'log10_p_val', 'color', 'index', 'p_val', 'annotated'];
@@ -74,110 +75,214 @@ const log10_pval_max = d3.max(data, (d,i) => {
 const log10_threshold = -Math.log10(significance_thresh);
 
 const y_max = options.y_max || Math.max(log10_threshold, log10_pval_max);
-const y = d3.scaleLinear()
-  .domain([0,y_max]).nice()
-  .range([height - margin.bottom, margin.top]);
 
-svg.append("g")
-  .call(function(g){
-    g.attr("transform", `translate(${margin.left},0)`)
-     .call(d3.axisLeft(y).tickSizeOuter(0));
-  });
-
-const x = d3.scaleLinear()
-  .domain([0, data.length])
-  .range([margin.left, width - margin.right]);
+const y = d3.scaleLinear().domain([0,y_max]).nice();
+const x = d3.scaleLinear().domain([0, data.length]);
 
 
-// scan through and assign bookkeeping props to the tooltips. 
-tooltips.forEach(d => {
-  d.x = x.invert(x(d.index) + tooltip_offset);
-  d.y = y.invert(y(d.log10_p_val) + tooltip_offset);
-});
+// Kick of the visualization
+drawPlot(width, height);
+r2d3.onResize(drawPlot);
 
+function drawPlot(width, height){
+  
+  svg.at({width,height});
+  
+  y.range([height - margin.bottom, margin.top]);
+  x.range([margin.left, width - margin.right]);
 
-
-const codes = svg.selectAppend('g.code_bubbles')
-  .selectAll('.code_bubble')
-  .data(data)
-  .enter().append('circle')
-  .at({
-    cx: d => x(d.index),
-    cy: d => y(d.log10_p_val),
-    fill: d => d.color,
-    r: point_size,
-  })
-  .on('click', addTooltip(false))
-  .on('mouseover', function(d){
-    //debugger;
-    popup
-     .st({
-       left: d3.event.clientX + tooltip_offset*2,
-       top: d3.event.clientY + tooltip_offset,
-       display: 'block'
-     })
-     .html(`<strong>${d.id}</strong>`);
-  })
-  .on('mouseout', function(d){
-    popup.style('display', 'none');
+  svg.selectAppend("y_axis")
+    .call(function(g){
+      g.attr("transform", `translate(${margin.left},0)`)
+       .call(d3.axisLeft(y).tickSizeOuter(0));
+    });
+    
+  // scan through and assign bookkeeping props to the tooltips. 
+  tooltips.forEach(d => {
+    if(!d.initialized){
+      d.x = x.invert(x(d.index) + tooltip_offset);
+      d.y = y.invert(y(d.log10_p_val) + tooltip_offset); 
+      d.initialized = true;
+    } 
   });
   
-// Draw significance line on y-axis
-significance_line = svg.selectAppend('g.significance_line')
-  .translate([0,y(-Math.log10(significance_thresh))]);
+  const codes = svg.selectAppend('g.code_bubbles')
+    .selectAll('.code_bubble')
+    .data(data, d => d.id);
+    
+  codes.enter()
+    .append('circle.code_bubble')
+    .merge(codes)
+    .at({
+      cx: d => x(d.index),
+      cy: d => y(d.log10_p_val),
+      fill: d => d.color,
+      r: point_size,
+    })
+    .on('click', addTooltip(false))
+    .on('mouseover', function(d){
+      popup
+       .st({
+         left: d3.event.clientX + tooltip_offset*2,
+         top: d3.event.clientY + tooltip_offset,
+         display: 'block'
+       })
+       .html(`<strong>${d.id}</strong>`);
+    })
+    .on('mouseout', function(d){
+      popup.style('display', 'none');
+    });
+    
+    
+  // Draw significance line on y-axis
+  significance_line = svg.selectAppend('g.significance_line')
+    .translate([0,y(-Math.log10(significance_thresh))]);
+    
+  significance_line.selectAppend('line')
+    .at({
+      x1: margin.left,
+      x2: width - margin.right,
+      stroke: 'black',
+      strokeWidth: 1,
+    });
+    
+  significance_line.selectAppend('text')
+    .at({
+      x: width - margin.right,
+      fontAlign: 'left',
+      fontSize: 18
+    })
+    .text(pval_formatter(significance_thresh));
   
-significance_line.selectAppend('line')
-  .at({
-    x1: margin.left,
-    x2: width - margin.right,
-    stroke: 'black',
-    strokeWidth: 1,
-  });
+  // axis labels
+  svg.selectAppend("text.y_axis_label")
+    .style('text-anchor', 'middle')
+    .at({
+      x: margin.left/2.2,
+      y: height/2,
+      fontSize: 18,
+      textAnchor: 'middle'
+    })
+    .html("-Log<tspan baseline-shift='sub' font-size=12>10</tspan>(P)");
   
-significance_line.selectAppend('text')
-  .at({
-    x: width - margin.right,
-    fontAlign: 'left',
-    fontSize: 18
-  })
-  .text(pval_formatter(significance_thresh));
-
-// axis labels
-svg.selectAppend("text.y_axis_label")
-  .style('text-anchor', 'middle')
-  .at({
-    x: margin.left/2.2,
-    y: height/2,
-    fontSize: 18,
-    textAnchor: 'middle'
-  })
-  .html("-Log<tspan baseline-shift='sub' font-size=12>10</tspan>(P)");
-
-svg.selectAppend("text.x_axis_label")
-  .style('text-anchor', 'middle')
-  .at({
-    x: width/2,
-    y: height,
-    fontSize: 18,
-    textAnchor: 'middle'
-  })
-  .text(options.x_axis);
-
-
-drawTooltips(tooltips);
-
-function addTooltip(pin){
-  return function(d){
-    d.y = y.invert(d3.event.clientY + tooltip_offset);
-    d.x = x.invert(d3.event.clientX + tooltip_offset);
+  svg.selectAppend("text.x_axis_label")
+    .style('text-anchor', 'middle')
+    .at({
+      x: width/2,
+      y: height,
+      fontSize: 18,
+      textAnchor: 'middle'
+    })
+    .text(options.x_axis);
    
-    // check if we already have an active tooltip for the selected code. 
-    if(!tooltips.find(code => code.id == d.id)){
-      tooltips.push(d);
-      drawTooltips(tooltips);  
-    }
-  };
+   drawTooltips(tooltips);
+
+   function addTooltip(pin){
+     return function(d){
+       d.y = y.invert(d3.event.clientY + tooltip_offset);
+       d.x = x.invert(d3.event.clientX + tooltip_offset);
+     
+       // check if we already have an active tooltip for the selected code. 
+       if(!tooltips.find(code => code.id == d.id)){
+         tooltips.push(d);
+         drawTooltips(tooltips);  
+       }
+     };
+  }
+  
+  function drawTooltips(tooltips){
+    const tooltip_lines = svg.selectAll('.tooltip_line')
+      .data(tooltips, d => d.id);
+      
+    tooltip_lines.enter().append('line.tooltip_line')
+      .merge(tooltip_lines)
+      .at({
+        x1: d => x(d.index),
+        y1: d => y(d.log10_p_val),
+        x2: d => x(d.x),
+        y2: d => y(d.y),
+        id: d => codeToId(d.id),
+        stroke: 'black',
+        strokeWidth: '1px',
+      })
+      .classed('tooltip_line', true);
+    
+    tooltip_lines.exit().remove();
+      
+    const tooltip_divs = div.selectAll('.annotation')
+      .data(tooltips, d => d.id);
+      
+    drawn_tips = tooltip_divs.enter()
+      .append('div.annotation')
+      .merge(tooltip_divs)
+      .st(tooltip_style)
+      .st({
+        top:d => `${y(d.y)}px`,
+        left:d => `${x(d.x)}px`,
+      })
+      .on('mouseover', function(){
+        d3.select(this).select('button').style('opacity', 1);
+      })
+      .on('mouseout', function(){
+        d3.select(this).select('button').style('opacity', 0);
+      })
+      .call(
+        d3.drag()
+          .on('start', function(d){
+            // record initial conditions of drag for later calculating movement.
+            drag_start = d3.event;
+            code_start = {x:x(d.x), y:y(d.y)};
+          })
+          .on('drag', function(d){
+            const drag_change = {
+              x: d3.event.x - drag_start.x,
+              y: d3.event.y - drag_start.y,
+            };
+            
+            const x_loc = code_start.x + drag_change.x;
+            const y_loc = code_start.y + drag_change.y;
+          
+            d.x = x.invert(x_loc);
+            d.y = y.invert(y_loc);
+            
+            console.log(`Code ${d.id} has an x value of ${d.x}`)
+  
+            d3.select(this).st({  
+              top: `${y_loc}px`,
+              left:`${x_loc}px`,
+            });
+            
+            svg.select(`#${codeToId(d.id)}`).at({  
+              x2: x_loc,
+              y2: y_loc,
+            });
+          })
+        );
+      
+     drawn_tips
+      .selectAppend('div.delete_button')
+      .st({
+        textAlign: 'right',
+        height: '10px'    
+      })
+        .selectAppend('button')
+        .text('X')
+        .st(delete_button_style)
+        .on('click', function(current){
+          const toDeleteIndex = tooltips.reduce((place, d, i) => d.id === current.id ? i : place, -1);
+          tooltips.splice(toDeleteIndex, 1);
+          svg.select(`#${codeToId(current.id)}`).remove();
+          drawTooltips(tooltips);
+        });
+      
+      drawn_tips.selectAppend('div.annotation_body')
+        .html(htmlFromProps);
+    
+    tooltip_divs.exit().remove(); 
+  }
+
 }
+
 
 function htmlFromProps(code){
   return Object.keys(code)
@@ -188,93 +293,7 @@ function htmlFromProps(code){
     );
 }
 
-function drawTooltips(tooltips){
-  
-  const tooltip_lines = svg.selectAll('.tooltip_line')
-    .data(tooltips, d => d.id);
-    
-  tooltip_lines.enter().append('line')
-    .at({
-      x1: d => x(d.index),
-      y1: d => y(d.log10_p_val),
-      x2: d => x(d.x),
-      y2: d => y(d.y),
-      id: d => codeToId(d.id),
-      stroke: 'black',
-      strokeWidth: '1px',
-    })
-    .classed('tooltip_line', true);
-  
-  tooltip_lines.exit().remove();
-    
-  const tooltip_divs = div.selectAll('.annotation')
-    .data(tooltips, d => d.id);
-    
-  const drawn_tips = tooltip_divs.enter()
-    .append('div.annotation')
-    .st(tooltip_style)
-    .st({
-      top:d => `${y(d.y)}px`,
-      left:d => `${x(d.x)}px`,
-    })
-    .on('mouseover', function(){
-      d3.select(this).select('button').style('opacity', 1);
-    })
-    .on('mouseout', function(){
-      d3.select(this).select('button').style('opacity', 0);
-    })
-    .call(
-      d3.drag()
-        .on('start', function(d){
-          // record initial conditions of drag for later calculating movement.
-          drag_start = d3.event;
-          code_start = {x:x(d.x), y:y(d.y)};
-        })
-        .on('drag', function(d){
-          const drag_change = {
-            x: d3.event.x - drag_start.x,
-            y: d3.event.y - drag_start.y,
-          };
-          
-          const x_loc = code_start.x + drag_change.x;
-          const y_loc = code_start.y + drag_change.y;
-        
-          d.x = x.invert(x_loc);
-          d.y = y.invert(y_loc);
 
-          d3.select(this).st({  
-            top: `${y_loc}px`,
-            left:`${x_loc}px`,
-          });
-          
-          svg.select(`#${codeToId(d.id)}`).at({  
-            x2: x_loc,
-            y2: y_loc,
-          });
-        })
-      );
-    
-   drawn_tips
-    .append('div')
-    .st({
-      textAlign: 'right',
-      height: '10px'    
-    })
-      .append('button')
-      .text('X')
-      .st(delete_button_style)
-      .on('click', function(current){
-        const toDeleteIndex = tooltips.reduce((place, d, i) => d.id === current.id ? i : place, -1);
-        tooltips.splice(toDeleteIndex, 1);
-        svg.select(`#${codeToId(current.id)}`).remove();
-        drawTooltips(tooltips);
-      });
-    
-    drawn_tips.append('div')
-      .html(htmlFromProps);
-  
-  tooltip_divs.exit().remove(); 
-}
 
 function codeToId(code){
   return `code_${code.replace('.', '_')}`;
